d4f3678e151e6e01da4dfd6f6dbd600c37e252cd
Luka <kazicluka4@gmail.com>
2026-07-07 22:24:33 +0200

    admin middleware

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->middleware('auth')->name('home');
 
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function () {
     Route::get('/', [WeatherController::class, 'index'])->name('dashboard');
     Route::get('/create-city', [WeatherController::class, 'create'])->name('create-city');
     Route::get('/edit-city', [WeatherController::class, 'edit'])->name('edit-city');
