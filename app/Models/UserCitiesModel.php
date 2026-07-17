b8ca6b0f953d23c05366f07455f15d750dc06599
lukakazic01 <kazicluka4@gmail.com>
2026-07-17 14:29:20 +0200

    defined city and user relationships inside user cities pivot model

--- app/Models/UserCitiesModel.php
+++ app/Models/UserCitiesModel.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[Table('user_cities')]
 #[Fillable(['user_id', 'city_id'])]
 class UserCitiesModel extends Model
 {
 
+    public function city(): BelongsTo {
+        return $this->belongsTo(CityModel::class, 'city_id', 'id');
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
