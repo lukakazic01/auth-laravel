2b66db157950dbc7f3e7819da428d913e884ba3e
Luka <kazicluka4@gmail.com>
2026-07-17 20:22:59 +0200

    Exercise 18 homework (fetch the current weather for entered city)

--- app/Console/Commands/GetRealWeather.php
+++ app/Console/Commands/GetRealWeather.php
@@ -16,7 +16,21 @@ class GetRealWeather extends Command
      */
     public function handle()
     {
-        $response = Http::get('https://api.weatherapi.com/v1/current.json?key=cee71b1b0f08435abfc112252222601&q=London&aqi=no');
-        dd($response->json());
+        $apiKey = env('WEATHER_API_KEY');
+        $cityName = $this->ask('Of which city weather are you interested in?');
+        if (empty($cityName)) {
+            $this->getOutput()->error('City name is empty');
+            return;
+        }
+        $response = Http::get("https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$cityName");
+        if ($response->successful()) {
+            $this->getOutput()->success("Weather data retrieved for $cityName!");
+            dd($response->json());
+        } else {
+            $errorMessage = $response->json()["error"]["message"];
+            $statusCode = $response->status();
+            $reason = $response->reason();
+            $this->getOutput()->error("There was an error :( \nStatus: $statusCode $reason \nMessage: $errorMessage");
+        }
     }
 }
--- app/Console/Commands/ReqResUser.php
+++ app/Console/Commands/ReqResUser.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
-#[Signature('reqres:get-user')]
+#[Signature('reqres:init')]
 #[Description('Command description')]
 class ReqResUser extends Command
 {
@@ -16,12 +16,12 @@ class ReqResUser extends Command
      */
     public function handle()
     {
-        $user = Http::withHeaders([
-            "x-api-key" => "free_user_3Gdjv9BRizp42oxx8DnS31lrAio",
-        ])->get('https://reqres.in/api/users/8');
+//        $user = Http::withHeaders([
+//            "x-api-key" => "free_user_3Gdjv9BRizp42oxx8DnS31lrAio",
+//        ])->get('https://reqres.in/api/users/655');
         $userCreate = Http::withHeaders([
             "x-api-key" => "free_user_3Gdjv9BRizp42oxx8DnS31lrAio"
-        ])->post("https://reqres.in/api/users/2", [
+        ])->post("https://reqres.in/api/users", [
             "name" => "Luka",
             "job" => "Full stack developer"
         ]);
