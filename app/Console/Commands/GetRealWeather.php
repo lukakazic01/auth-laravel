6bff906866311d0eef63f2634f2c18f11bc3c45e
lukakazic01 <kazicluka4@gmail.com>
2026-07-20 12:11:14 +0200

    Exercise 19 exercises

--- app/Console/Commands/GetRealWeather.php
+++ app/Console/Commands/GetRealWeather.php
@@ -5,26 +5,28 @@ namespace App\Console\Commands;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 
-#[Signature('weather:get-real')]
+#[Signature('weather:get-real {city}')]
 #[Description('Command description')]
 class GetRealWeather extends Command
 {
     /**
      * Execute the console command.
+     * @throws ConnectionException
      */
-    public function handle()
+    public function handle(): void
     {
-        $apiKey = env('WEATHER_API_KEY');
-        $cityName = $this->ask('Of which city weather are you interested in?');
-        if (empty($cityName)) {
-            $this->getOutput()->error('City name is empty');
-            return;
-        }
-        $response = Http::get("https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$cityName");
+        $city = $this->argument('city');
+        $response = Http::withOptions(['verify' => false])->get(env('WEATHER_API_URL')."/v1/forecast.json", [
+            "key" => env('WEATHER_API_KEY'),
+            "q" => $city,
+            "days" => 5,
+            "lang" => "en"
+        ]);
         if ($response->successful()) {
-            $this->getOutput()->success("Weather data retrieved for $cityName!");
+            $this->getOutput()->success("Weather data retrieved for $city!");
             dd($response->json());
         } else {
             $errorMessage = $response->json()["error"]["message"];
